402c60552c26ebaad6a5a6dd65c3fdb88d4c19e9
Wenson Tsai <wensontsai@gmail.com>
2013-10-28 16:34:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripplanner::Application.config.secret_key_base = '263e7831d0b99eac12629fcbff9ea24d3ab0f91e783bc647520a516f180afa4f62d882b85d4ad6e6cc40bee5e3db95443438777605d3773068488e0f9bd23fa3'
